ec61cdc283f29e1ba23e5a10e940bdadf299abf7
Lars Strange Hagen <lars.strange.hagen@lego.com>
2022-12-05 21:43:30 +0100

    Day 5 part 2

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -5,7 +5,8 @@ using System.Linq;
 
 Console.WriteLine("Hello, World!");
 
-List<Stack<char>> crateStacks = new();
+List<Stack<char>> part1Stacks = new();
+List<Stack<char>> part2Stacks = new();
 
 var allLines = File.ReadLines("Input.txt").ToList();
 
@@ -15,8 +16,11 @@ var stackCountLineIndex = allLines.IndexOf(stackCountLine);
 var stackCount = int.Parse(stackCountLine[^2].ToString());
 
 //Init stacks
-for (int i = 0; i< stackCount;i++)
-    crateStacks.Add(new());
+for (int i = 0; i < stackCount; i++)
+{
+    part1Stacks.Add(new());
+    part2Stacks.Add(new());
+}
 
 //Parse stacks
 for (int i = stackCountLineIndex - 1; i >= 0; i--)
@@ -27,11 +31,13 @@ for (int i = stackCountLineIndex - 1; i >= 0; i--)
         if (target == ' ')
             continue;
         
-        crateStacks[j].Push(target);
+        part1Stacks[j].Push(target);
+        part2Stacks[j].Push(target);
     }
 }
 
 //Parse and execute moves
+Stack<char> tempStackForPart2 = new();
 for (int i = stackCountLineIndex + 2; i < allLines.Count; i++)
 {
     var split = allLines[i].Split(' ');
@@ -39,17 +45,30 @@ for (int i = stackCountLineIndex + 2; i < allLines.Count; i++)
     var from = int.Parse(split[3]) - 1;
     var to = int.Parse(split[5]) - 1;
 
+    
     for (int j = 0; j < amount; j++)
     {
-        crateStacks[to].Push(crateStacks[from].Pop());
+        part1Stacks[to].Push(part1Stacks[from].Pop());
+        tempStackForPart2.Push(part2Stacks[from].Pop());
+    }
+
+    while (tempStackForPart2.Count > 0)
+    {
+        part2Stacks[to].Push(tempStackForPart2.Pop());
     }
 }
 
 //Get result
 string result = "";
-foreach (var stack in crateStacks)
+foreach (var stack in part1Stacks)
 {
     result += stack.Peek();
 }
-
 Console.WriteLine("Part 1: " + result);
+
+result = "";
+foreach (var stack in part2Stacks)
+{
+    result += stack.Peek();
+}
+Console.WriteLine("Part 2: " + result);
