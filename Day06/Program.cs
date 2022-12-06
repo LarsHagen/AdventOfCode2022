62ea065941a33748dd88bfa9be024706efb363a0
Lars Strange Hagen <lars.strange.hagen@lego.com>
2022-12-06 20:19:14 +0100

    Day 6

---
+++ Day06/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+int GetMarkerIndex(string s, int packageSize)
+{
+    for (int i = 0; i < s.Length - packageSize; i++)
+    {
+        var subString = s.Substring(i, packageSize);
+        if (subString.Distinct().Count() == packageSize)
+            return i + packageSize;
+    }
+
+    throw new Exception("This should not happen");
+}
+
+Console.WriteLine("Day 6");
+
+var input = File.ReadAllText("Input.txt");
+
+Console.WriteLine("Part 1: " + GetMarkerIndex(input, 4));
+Console.WriteLine("Part 2: " + GetMarkerIndex(input, 14));
