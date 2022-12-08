634a217beeb49fc535771677e1586aaad43fbb60
Lars Strange Hagen <lars.strange.hagen@lego.com>
2022-12-08 10:36:30 +0100

    Day 8 part 2

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -92,3 +92,57 @@ for (int y = 0; y < height; y++)
 }
 
 Console.WriteLine("Part 1: " + visible.Count);
+
+//Find tree with best view
+int bestView = 0;
+for (int y = 0; y < height; y++)
+{
+    for (int x = 0; x < width; x++)
+    {
+        int treeHeight = map[x, y];
+        
+        int leftDistance = 0;
+        for (int left = x - 1; left >= 0; left--)
+        {
+            leftDistance++;
+            
+            if (map[left, y] >= treeHeight)
+                break;
+        }
+        
+        int rightDistance = 0;
+        for (int right = x + 1; right < width; right++)
+        {
+            rightDistance++;
+            
+            if (map[right, y] >= treeHeight)
+                break;
+        }
+        
+        int downDistance = 0;
+        for (int down = y - 1; down >= 0; down--)
+        {
+            downDistance++;
+            
+            if (map[x, down] >= treeHeight)
+                break;
+        }
+        
+        int upDistance = 0;
+        for (int up = y + 1; up < height; up++)
+        {
+            upDistance++;
+            
+            if (map[x, up] >= treeHeight)
+                break;
+        }
+
+        int viewScore = leftDistance * rightDistance * upDistance * downDistance;
+        if (viewScore > bestView)
+        {
+            bestView = viewScore;
+        }
+    }
+}
+
+Console.WriteLine("Part 2: " + bestView);
