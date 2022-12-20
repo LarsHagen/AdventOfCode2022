d924f5a0c6947646a30f4b0abdd4a08313d1e677
Lars Strange Hagen <lars.strange.hagen@lego.com>
2022-12-20 08:58:44 +0100

    Day 20 part 1

---
+++ Day20/Node.cs
@@ -0,0 +1,57 @@
+namespace Day20
+{
+    public class Node
+    {
+        public Node Previous;
+        public Node Next;
+        public int Value;
+        public static Node Start;
+
+        public void MoveForward()
+        {
+            if (Start == Next)
+            {
+                //Start = this;
+            }
+            else if (Start == this)
+            {
+                Start = Next;
+            }
+
+            var p = Next;
+            var n = Next.Next;
+            
+            Next.Previous = Previous;
+            Previous.Next = Next;
+            Next.Next.Previous = this;
+            Next.Next = this;
+
+            Previous = p;
+            Next = n;
+        }
+
+        public void MoveBack()
+        {
+            
+            if (Start == Previous)
+            {
+                //Start = this;
+            }
+            else if (Start == this)
+            {
+                Start = Next;
+            }
+            
+            var p = Previous.Previous;
+            var n = Previous;
+            
+            Next.Previous = Previous;
+            Previous.Next = Next;
+            Previous.Previous.Next = this;
+            Previous.Previous = this;
+
+            Previous = p;
+            Next = n;
+        }
+    }
+}
---
+++ Day20/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Day20;
+
+Console.WriteLine("Day 20");
+
+
+var lines = File.ReadAllLines("Input.txt");
+
+List<Node> allNodes = new();
+
+foreach (var t in lines)
+{
+    allNodes.Add(new Node()
+    {
+        Value = int.Parse(t)
+    });
+}
+
+for (int i = 0; i < allNodes.Count; i++)
+{
+    if (i == 0)
+    {
+        allNodes[i].Previous = allNodes[^1];
+        allNodes[i].Next = allNodes[i + 1];
+        Node.Start = allNodes[i];
+    }
+    else if (i == allNodes.Count - 1)
+    {
+        allNodes[i].Previous = allNodes[i - 1];
+        allNodes[i].Next = allNodes[0];
+    }
+    else
+    {
+        allNodes[i].Previous = allNodes[i - 1];
+        allNodes[i].Next = allNodes[i + 1];
+    }
+}
+
+HashSet<Node> burnedNodes = new();
+
+while (burnedNodes.Count < allNodes.Count)
+{
+    Node current = Node.Start;
+
+    while (burnedNodes.Contains(current))
+        current = current.Next;
+
+    burnedNodes.Add(current);
+
+    int instruction = current.Value;
+
+    for (int i = 0; i < Math.Abs(instruction); i++)
+    {
+        if (instruction < 0)
+            current.MoveBack();
+        else
+            current.MoveForward();
+    }
+    
+    //Print
+    /*Node printNode = Node.Start;
+    string output = "";
+    do
+    {
+        output += printNode.Value + ", ";
+        printNode = printNode.Next;
+    } while (printNode != Node.Start);*/
+    string output = $"{burnedNodes.Count}/{allNodes.Count}";
+    Console.WriteLine(output);
+}
+
+int sum = 0;
+Node iteration = Node.Start;
+while (iteration.Value != 0)
+    iteration = iteration.Next;
+for (int i = 1; i <= 3000; i++)
+{
+    iteration = iteration.Next;
+    if (i % 1000 == 0)
+    {
+        Console.WriteLine(iteration.Value);
+        sum += iteration.Value;
+    }
+}
+Console.WriteLine("Part 1: " + sum);
